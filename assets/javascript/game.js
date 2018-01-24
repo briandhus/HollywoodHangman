ea948f3242e8513dfa933d9b0e23579f5ca6b529
Brian Huston <hbriandrums@gmail.com>
2018-01-23 17:03:22 -0800

    record wrong guesses, favicon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,37 +3,56 @@ var titles = ["better off dead", "the goonies", "star wars", "gremlins", "aliens
 var renderedString = "";
 var wins = 0;
 var losses = 0;
-var ties = 0;
-var guessedLetters = [];
+var wrongGuesses = [];
+var correctLetters = [];
+var lives = 7;
 
+// letter bank
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    
 // Choose a random title
 var randomTitle = titles[Math.floor(Math.random() * titles.length)];
 console.log(randomTitle);
 
 // computer renders length of title in underscores
-for (var i = 0; i < randomTitle.length; i++) {
-	if (randomTitle[i] === " ") {
-		renderedString += " ";		
-	} else {
-		renderedString += "_";
-	}	
-}
-
-// rendered title displays on screen
-window.onload = function displayRenderedString() {
+function displayRenderedString() {
+	for (var i = 0; i < randomTitle.length; i++) {
+		if (randomTitle[i] === " ") {
+			renderedString += " ";		
+		} else {
+			renderedString += "_";
+		}	
+	}
 	var html = '<p>' + renderedString + '</p>';
 	document.getElementById('movieTitle').innerHTML = html;
 }
 
-// displayRenderedString();
+displayRenderedString();
 console.log(renderedString);
 
-// letter bank
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // capture user guess
 document.onkeyup = function() {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(userGuess);
+
+	if (randomTitle.indexOf(userGuess) === -1 && letters.indexOf(userGuess) >= 0 && wrongGuesses.indexOf(userGuess) === - 1) {
+		wrongGuesses.push(userGuess);
+
+		var displayWrongGuesses = document.getElementById('wrongGuesses');
+		displayWrongGuesses.innerHTML = wrongGuesses.join(', ').toUpperCase();
+	}
+
+	if (randomTitle.indexOf(userGuess) >= 0 && randomTitle.indexOf(userGuess) < randomTitle.length && correctLetters.indexOf(userGuess) === -1) {
+		correctLetters.push(userGuess);
+
+		var displayCorrectLetters = document.getElementById('movieTitle');
+		displayCorrectLetters.innerHTML = correctLetters.join(' ').toUpperCase();
+	}
 }
+
+
+
+
+
